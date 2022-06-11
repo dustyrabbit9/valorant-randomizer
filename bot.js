bad60102c774f67ff7334fdbe07818c57219a297
Imran Rasheed <imran.2001r@gmail.com>
2022-06-11 08:30:42 +0530

    Initial commit

---
+++ bot.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const Discord = require("discord.js");
+const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+client.on("message", msg => {
+  if (msg.content === "ping") {
+    msg.reply("pong");
+  }
+})
+client.on("message", msg => {
+  if (msg.content === "big") {
+    msg.reply("bang");
+  }
+})
+client.login(process.env.DISCORD_TOKEN);
